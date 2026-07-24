253b803a8b6a330e01a3dcccfdaccbb912201450
c2ck6kknpb-max <c2ck6kknpb@privaterelay.appleid.com>
2026-07-24 05:28:49 +0300

    Create data.js

---
+++ js/data.js
@@ -0,0 +1,23 @@
+const businesses = [
+  {
+    name: "مطعم الشام",
+    category: "مطعم",
+    city: "دمشق",
+    rating: 4.9,
+    image: "https://images.unsplash.com/photo-1517248135467-4c7edcad34c4?w=800"
+  },
+  {
+    name: "صيدلية الأمل",
+    category: "صيدلية",
+    city: "حلب",
+    rating: 4.8,
+    image: "https://images.unsplash.com/photo-1580281657527-47f249e8f4df?w=800"
+  },
+  {
+    name: "ورشة دمشق",
+    category: "ورشة",
+    city: "دمشق",
+    rating: 4.7,
+    image: "https://images.unsplash.com/photo-1486006920555-c77dcf18193c?w=800"
+  }
+];
