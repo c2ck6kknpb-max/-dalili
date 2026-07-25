777ca492471ca323e8e185e4313d1161a3119396
c2ck6kknpb-max <c2ck6kknpb@privaterelay.appleid.com>
2026-07-25 19:39:27 +0300

    Update login.js

--- js/login.js
+++ js/login.js
@@ -1,49 +1,5 @@
-console.log("login.js يعمل");
-
-import { auth } from "./firebase.js";
-
-import {
-signInWithEmailAndPassword
-} from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
-
-
 document.getElementById("loginBtn").onclick = function(){
 
 alert("الزر يعمل");
-
-};
-
-let email =
-document.getElementById("email").value;
-
-
-let password =
-document.getElementById("password").value;
-
-
-
-try{
-
-
-await signInWithEmailAndPassword(
-auth,
-email,
-password
-);
-
-
-alert("تم تسجيل الدخول");
-
-
-location.href="profile.html";
-
-
-
-}catch(error){
-
-alert("البريد أو كلمة المرور غير صحيحة");
-
-}
-
 
 };
